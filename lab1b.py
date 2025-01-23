eb46409c1de2cdec0d84ea889e4f53947e124684
prakash <10prakashgautam@gmail.com>
2025-01-23 11:04:41 -0500

    completed lab

---
+++ lab1b.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Define a string object 'name'
+name = 'Isaac'
+
+# Print out a sentence using string concatenation
+print('How old are you ' + name + '?')
---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define integer objects
+x = 10
+y = 2
+z = 5
+
+# Perform and print the math operation
+result = x + y * z  # according to PEMDAS, multiplication happens first
+
+# Print the result in the format requested
+print(f"{x} + {y} * {z} = {result}")
